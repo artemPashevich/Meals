56e732e33e58d880d99f8bfe112a36ba5c3d96a2
artemPashevich <100874372+artemPashevich@users.noreply.github.com>
2022-08-23 02:13:05 +0300

    Custom Cell and preview

---
+++ MealsApp/MealsApp/CustomMealsTVCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomMealsTVCell.swift
+//  MealsApp
+//
+//  Created by Артем Пашевич on 23.08.22.
+//
+
+import UIKit
+
+class CustomMealsTVCell: UITableViewCell {
+
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var raitingLbl: UILabel!
+    @IBOutlet weak var subTitleLbl: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
